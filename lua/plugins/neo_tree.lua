b3feeccdd5eefb025306d2a7110c753f611397dd
leaf48 <watavita2004@gmail.com>
2025-02-23 01:07:51 -0800

    fix: show system files

--- lua/plugins/neo_tree.lua
+++ lua/plugins/neo_tree.lua
@@ -1,13 +1,19 @@
-
 return {
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    dependencies={
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons",
-      "MunifTanjim/nui.nvim",
-      "3rd/image.nvim"
-    },
+  "nvim-neo-tree/neo-tree.nvim",
+  dependencies={
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+    "3rd/image.nvim"
+  },
+  opts = {
+    filesystem= {
+      filtered_items = {
+        visible = true,
+        hide_dotfiles = false,
+        hide_gitignored = false,
+      }
+    }
   }
 }
 
