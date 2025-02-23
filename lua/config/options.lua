43dd86dfc7b532786ec364ad05eb0b86cd11274e
leaf48 <watavita2004@gmail.com>
2025-02-23 00:14:42 -0800

    feat: options

---
+++ lua/config/options.lua
@@ -0,0 +1,30 @@
+-- leader
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- enable numbers 
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- mouse input
+vim.opt.mouse = "a"
+
+-- clipboard
+vim.opt.clipboard = "unnamedplus"
+
+-- encoding 
+vim.opt.encoding = "utf-8"
+
+-- color schemes
+vim.opt.termguicolors = true
+vim.cmd("syntax on")
+
+-- search settings
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- indent
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.smartindent = true
