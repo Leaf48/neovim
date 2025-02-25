1d185f3a7c85326213b09e9f4d185812729d77bb
leaf48 <watavita2004@gmail.com>
2025-02-25 00:50:16 -0800

    fix: conflicted keys

--- lua/config/keymaps/basic.lua
+++ lua/config/keymaps/basic.lua
@@ -4,7 +4,7 @@ keymap.set("n", "<Leader>sa", ":wa<CR>", opt)
 keymap.set("n", "<Leader>q", ":qa<CR>", opt)
 
 -- buffer close
-keymap.set("n", "bc", ":BufferClose!<CR>", opt)
+keymap.set("n", "<Leader>bc", ":BufferClose!<CR>", opt)
 
 -- docs
 local wk = require('which-key')
@@ -12,6 +12,7 @@ wk.add({
   {"jk", mode = "i", desc = "Escape"},
   {"<Leader>sa", mode = "n", desc = "Save all"},
   {"<Leader>q", mode = "n", desc = "Quit all"},
+  {"<Leader>bc", mode = "n", desc = "Buffer Close"},
 })
 
 
--- lua/plugins/lspsaga.lua
+++ lua/plugins/lspsaga.lua
@@ -9,20 +9,20 @@ return {
     })
 
     -- show hover doc
-    vim.keymap.set("n", "hd", ":Lspsaga hover_doc<CR>", opt)
+    vim.keymap.set("n", "<C-c>h", ":Lspsaga hover_doc<CR>", opt)
     -- rename variable
-    vim.keymap.set("n", "R", ":Lspsaga rename<CR>", opt)
+    vim.keymap.set("n", "<C-c>r", ":Lspsaga rename<CR>", opt)
     -- go to definition
-    vim.keymap.set("n", "gd", ":Lspsaga goto_definition<CR>", opt)
+    vim.keymap.set("n", "<C-c>gd", ":Lspsaga goto_definition<CR>", opt)
     -- see references
-    vim.keymap.set("n", "sr", ":Lspsaga finder<CR>", opt)
+    vim.keymap.set("n", "<C-c>sr", ":Lspsaga finder<CR>", opt)
 
     local wk = require('which-key')
     wk.add({
-      {"hd", mode = "n", desc = "Hover document"},
-      {"R", mode = "n", desc = "Rename variable/file"},
-      {"gd", mode = "n", desc = "Go to definition"},
-      {"sr", mode = "n", desc = "See references"},
+      {"<C-c>h", mode = "n", desc = "Hover document"},
+      {"<C-c>r", mode = "n", desc = "Rename variable/file"},
+      {"<C-c>gd", mode = "n", desc = "Go to definition"},
+      {"<C-c>sr", mode = "n", desc = "See references"},
     })
   end,
   dependencies = {
